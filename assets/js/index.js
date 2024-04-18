b65c39626b9a2a796de61a235a27b0176c054e49
oliverudekyll <oliverudekll@gmail.com>
2024-04-18 17:42:47 +0200

    pre translate checkout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,35 @@
-const helloWorld = () => {
-    console.log("Hello World");
-  };
-  
-  helloWorld();
+const cursorCaption = document.getElementById("cursor-caption");
+
+document.addEventListener("mousemove", function (event) {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  cursorCaption.style.left = `${mouseX - cursorCaption.clientWidth / 2}px`;
+  cursorCaption.style.top = `${mouseY - cursorCaption.clientHeight / 2}px`;
+});
+
+const contentImages = document.querySelectorAll(".content__img");
+
+contentImages.forEach(function (image) {
+  image.addEventListener("mouseover", function (event) {
+    const imageCaption = event.target.dataset.caption;
+
+    /* cursorCaption.style.display = "block"; */
+    cursorCaption.innerText = imageCaption;
+    cursorCaption.style.fontSize = "";
+    onmouseout = (event) => {
+      /* cursorCaption.style.display = "none"; */
+      cursorCaption.innerText = "ʚ♡ɞ";
+      cursorCaption.style.fontSize = "1.5rem";
+    };
+  });
+});
+
+window.addEventListener("scroll", () => {
+  contentImages.forEach(function (image) {
+    let rect = image.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+      image.style.transform = "translate(0)";
+      /*       image.style.filter = "grayscale(0) contrast(1) brightness(1)"; */
+    }
+  });
+});
