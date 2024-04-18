87d1f6f6fbe6d9b7c57d3d583a43a1e541da5b3a
oliverudekyll <oliverudekll@gmail.com>
2024-04-18 21:02:52 +0200

    first prototype

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,16 @@
 const cursorCaption = document.getElementById("cursor-caption");
+const cursor = document.getElementById("cursor-caption_cursor");
+const caption = document.getElementById("cursor-caption_caption");
 
-document.addEventListener("mousemove", function (event) {
+const cursorMove = function (event) {
   const mouseX = event.clientX;
   const mouseY = event.clientY;
   cursorCaption.style.left = `${mouseX - cursorCaption.clientWidth / 2}px`;
   cursorCaption.style.top = `${mouseY - cursorCaption.clientHeight / 2}px`;
-});
+};
+
+document.addEventListener("DOMContentLoaded", cursorMove);
+document.addEventListener("mousemove", cursorMove);
 
 const contentImages = document.querySelectorAll(".content__img");
 
@@ -13,18 +18,34 @@ contentImages.forEach(function (image) {
   image.addEventListener("mouseover", function (event) {
     const imageCaption = event.target.dataset.caption;
 
-    /* cursorCaption.style.display = "block"; */
-    cursorCaption.innerText = imageCaption;
-    cursorCaption.style.fontSize = "";
+    caption.innerText = imageCaption;
+    caption.style.opacity = "1";
+    cursor.style.width = "0";
+    cursor.style.height = "0";
+    cursor.style.opacity = "0";
     onmouseout = (event) => {
-      /* cursorCaption.style.display = "none"; */
-      cursorCaption.innerText = "ʚ♡ɞ";
-      cursorCaption.style.fontSize = "1.5rem";
+      caption.style.opacity = "0";
+      cursor.style.width = "";
+      cursor.style.height = "";
+      cursor.style.opacity = "";
     };
   });
 });
 
+const preface = document.getElementById("preface");
+const prefaceAbout = document.getElementById("preface__about");
+
 window.addEventListener("scroll", () => {
+  let prefaceRect = preface.getBoundingClientRect();
+  /* prefaceAbout.style.opacity = `${prefaceRect.bottom / window.innerHeight}`; */
+  if (prefaceAbout.clientHeight >= prefaceRect.bottom) {
+    prefaceAbout.style.transform = `translateY(-${prefaceAbout.clientHeight}px)`;
+    prefaceAbout.style.opacity = "0";
+  } else {
+    prefaceAbout.style.transform = "";
+    prefaceAbout.style.opacity = "1";
+  }
+
   contentImages.forEach(function (image) {
     let rect = image.getBoundingClientRect();
     if (rect.top < window.innerHeight) {
